a721081ad7004e408e130e1d1ecddd931569a244
khoabui <buidangkhoa05@gmail.com>
2024-02-20 14:07:17 +0700

    docs : add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,34 @@
-import { NestFactory } from '@nestjs/core';
-import { ApiModule } from './api.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import {resolve} from "path"
-import {SocketIoAdapter} from "@secretlab/socket"
-import { AuthGuard } from 'auth.guard';
-import { JwtService } from '@nestjs/jwt';
-(BigInt.prototype as any).toJSON = function() {
-  return this.toString()
+import { NestFactory } from "@nestjs/core";
+import { ApiModule } from "./api.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { resolve } from "path";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
+function useSwagger(app: NestExpressApplication) {
+  const config = new DocumentBuilder()
+    .setTitle("Cats example")
+    .setDescription("The cats API description")
+    .setVersion("1.0")
+    .addTag("cats")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
 }
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(ApiModule);
-  app.useStaticAssets(resolve(__dirname, '..', 'public'));
+  
+  useSwagger(app);
+
+  app.useStaticAssets(resolve(__dirname, "..", "public"));
   app.enableCors({
     origin: ["http://localhost:5173"],
-    methods: "*"
+    methods: "*",
   });
+
   // app.useWebSocketAdapter(new SocketIoAdapter(app))
   await app.listen(4000);
 }
